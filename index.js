6e54a6c1d4555749c3a6cd5bea4cf46880087ecd
alekseygurenkov <aleksey.sknt@gmail.com>
2025-02-09 19:56:04 +0300

    дописал css, мелкие доработки html, скачал по новой с макета картинки стрелки и закрытия без масштабирования, js добавлены константы

---
+++ index.js
@@ -0,0 +1,3 @@
+const inputNode = document.querySelector('.js-add_movies-name');
+const btnInputNode = document.querySelector ('.js-btn_add-movies');
+const listNode = document.querySelector('.js-movie_list');
